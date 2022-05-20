a3e0f3570fb333cdda08bd11d1f9d475fcecfe91
sent1nu11 <sent1nu11@gmail.com>
2022-05-20 07:57:47 -0600

    update to layout

---
+++ ConcurWorkdayAnalyzer.py
@@ -0,0 +1,30 @@
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+
+Window.size = (1000, 800)
+
+class MainWidget(GridLayout):
+    def on_button_click(self):
+        print("Button clicked")
+
+
+    # def selected(self, filename):
+    #     try:
+    #         self.ids.my_file.source = filename[0]
+    #         print(filename[0])
+    #
+    #     except:
+    #         pass
+
+
+class ConcurWorkday(App):
+    def build(self):
+        return MainWidget()
+
+
+if __name__ == "__main__":
+    window = ConcurWorkday()
+    window.run()
---
+++ temp/ConcurWorkdayAnalyzer.py
@@ -0,0 +1,25 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+
+
+class MyWidget(GridLayout):
+
+    def selected(self, filename):
+        try:
+            self.ids.my_file.source = filename[0]
+            print(filename[0])
+
+        except:
+            pass
+
+
+class FileChooserWindow(App):
+    def build(self):
+        return MyWidget()
+
+
+if __name__ == "__main__":
+    window = FileChooserWindow()
+    window.run()
